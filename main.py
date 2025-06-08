bb65da29ae3b84c1d715ed2892263795974022f0
jay940721 <mickey.chou2005@gmail.com>
2025-06-09 00:23:01 +0800

    refactor: 重構主選單邏輯

--- main.py
+++ main.py
@@ -7,36 +7,43 @@ def main():
     while True:
         os.system("cls" if os.name == "nt" else "clear")
         print("\n歡迎使用收支紀錄系統")
-        print("recode 記錄收支")
-        print("view 查看收支")
-        print("delete 刪除收支")
+        print("expe 收支")
         print("budget 預算")
         print("exit 離開")
-        choice = input(">> ")
+        choice = input(">> ").strip().lower().split()
 
-        if choice == "recode":
-            expense.record()
-        elif choice == "view":
-            expense.view()
-        elif choice == "delete":
-            expense.delete()
-        elif choice == "budget":
-            print("set - 設定預算")
-            print("view - 查看預算")
-            choice = input()
-            if choice == "set" :
+        if choice[0] == "expe":
+            if len(choice) == 1:
+                print("recode 記錄收支")
+                print("view 查看收支")
+                print("delete 刪除收支")
+                choice.append(input(">> ").strip().lower())
+            if choice[1] == "recode":
+                expense.record()
+            elif choice[1] == "view":
+                expense.view()
+            elif choice[1] == "delete":
+                expense.delete()
+            else:
+                print(f"{choice[1]} 不是一個正確的選項，請重新輸入選項。")
+        elif choice[0] == "budget":
+            if len(choice) == 1:
+                print("set 設定預算")
+                print("view 查看預算")
+                choice.append(input(">> ").strip().lower())
+            if choice[1] == "set" :
                 month = input("請輸入年份-月份(YYYY-MM): ").strip()
                 budget.set_monthly_budget(month)
-            elif choice == "view":
+            elif choice[1] == "view":
                 month = input("請輸入年份-月份(YYYY-MM): ").strip()
                 budget.get_monthly_budget(month)
             else:
-                print(f"{choice} 不是一個正確的選項，請重新輸入選項。")
-        elif choice == "exit" or choice == "quit":
+                print(f"{choice[1]} 不是一個正確的選項，請重新輸入選項。")
+        elif choice[0] == "exit":
             print("再見！")
             break
         else:
-            print(f"{choice}不是一個正確的選項，請重新輸入選項。")
+            print(f"{choice[0]}不是一個正確的選項，請重新輸入選項。")
 
 
 if __name__ == "__main__":
